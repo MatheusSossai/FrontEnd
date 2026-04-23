bc9a465c1382cf6c5bfee91f1110d2a21a25c120
Matheus B. Sossai <matheussossai18@gmail.com>
2026-04-23 03:04:50 -0300

    Add files via upload

---
+++ prova3/blabla.js
@@ -0,0 +1,25 @@
+function revelar() {
+   
+    const img = document.getElementById("imagem");
+    img.src = "vinicraque.png";
+
+  
+    const nome = document.getElementById("nome");
+    const data = document.getElementById("data");
+    const altura = document.getElementById("altura");
+    const posicao = document.getElementById("posicao");
+    const rank = document.getElementById("rank");
+
+    nome.textContent = "Vinícius José Paixão de Oliveira Júnior";
+    data.textContent = "12/07/2000 (25 anos)";
+    altura.textContent = "1,76 m";
+    posicao.textContent = "Ponta-esquerda / Atacante";
+    rank.textContent = "9,5";
+
+    const elementos = [nome, data, altura, posicao, rank];
+
+    elementos.forEach(el => {
+        el.classList.remove("placeholder");
+        el.classList.add("card-text");
+    });
+}
